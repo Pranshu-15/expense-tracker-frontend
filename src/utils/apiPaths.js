a3b175d4a33d4bfb892b2bcb12850b1f83e78ee9
Pranshu-15 <pranshu923@gmail.com>
2025-06-09 00:25:12 +0530

    final

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "http://localhost:8000"|| "https://expense-tracker-backend-m3cs.onrender.com";
 
 // utils/apiPaths.js
 export const API_PATHS = {
